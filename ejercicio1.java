18ac09dd99f8defd3917e61c468320ed26c940f6
Alex Camilo Hernández Piragauta <alexcamilohernandezpiragauta@gmail.com>
2023-07-27 16:46:54 -0500

    Segundo

---
+++ ejercicio1.java
@@ -0,0 +1,15 @@
+import java.util.scanner;
+public class ejercicio1 {   
+public static void main(String [] args ){ 
+
+ int n1 
+ for (int i=1;i <101;i++){
+if (i%3==0){
+System.out.println (el numero"+i+" es divisible por 3");}
+         
+    }
+  }
+}
+
+    
+  
